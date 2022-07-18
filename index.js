f3dcb9eed22a20ec92a39f7a4c157e97b2978591
Mikko Virpi <t9vimi04@students.oamk.fi>
2022-07-18 23:24:17 +0300

    tehtävät 3.17 ja 3.18

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ app.delete('/api/persons/:id', morgan('tiny'), (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', morgan('tiny'), (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.post('/api/persons', morgan(':method :url :status :bodyJSON'), (request, response, next) => {
   const body = request.body
   if (body.name === undefined) {
